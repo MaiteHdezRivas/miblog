1cb1a16382eaf43af2ffedf79949388ef4a3f9df
MaiteHdezRivas <maite.hernandez@gmail.com>
2017-11-16 10:27:59 +0100

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'estaticas/home'
+
+  get 'estaticas/index'
+
+  get 'home/index'
+  root 'home#index'
+  
+  # For details on rathe DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
